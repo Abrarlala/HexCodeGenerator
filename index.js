39461728ba5f43a51dfc541af65ab33a349af376
Abrar Hussain <136314551+Abrarlala@users.noreply.github.com>
2023-09-26 17:56:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+let color;
 function randomColor() {
     const hex = '0123456789ABCDEF';
-    let color = '#';
+    color = '#';
     for (let i = 0; i < 6; i++) {
         color += hex[Math.floor(Math.random() * 16)];
     }
@@ -12,7 +13,7 @@ let remove;
 function startChangeColor() {
     remove = setInterval(() => {
         document.body.style.backgroundColor = randomColor();
-        document.querySelector(".hex-code").innerHTML = randomColor();
+        document.querySelector(".hex-code").innerHTML = color;
     }, 1500);
 
 }
